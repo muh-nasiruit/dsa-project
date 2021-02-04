de903877d9b09b40ea55eee620f4aa91c07bdcdc
Muhammad Nasir <muhnasir@students.uit.edu>
2021-02-05 01:48:59 +0500

    Update DSA Project.py

--- DSA Project.py
+++ DSA Project.py
@@ -5,20 +5,31 @@ from math import *
 class Node:
     def __init__(self, p):
         self.value = p
-        self.pl = None
-        self.pr = None
-#root.left = pl
-#root.right = pr
+        self.LCHILD = None
+        self.RCHILD = None
+        self.LRADIUS = 0
+        self.RRADIUS = 0
+        self.FULL = False
+    
+    def TempStorage(self, direction):
+        direction = 
+        
+#root.left = LCHILD
+#root.right = RCHILD
+#NODEPOINTER = self.root
 class BST:
     def __init__(self):
         self.root = None
+        self.direction = None
         
     def distance(self, p1, p2):
         dist = sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
         res = float('{:.2}'.format(dist))
         return res
         
-        
+    def Decision(self, direction):
+        self.direction = direction
+    
     def Insert(self, value):
         #wrapper function encapsulates the implementation technique
         self.root = self.__Insert(self.root, value)
@@ -30,11 +41,11 @@ class BST:
             
         else:
             #instead of value it will be self.distance(value, root.right) i guess
-            if value > root.value:
-                root.right = self.__Insert(root.right, value)
+            if self.distance(value > root.value:
+                root.pr = self.__Insert(root.pl, value)
             
             else:
-                root.left = self.__Insert(root.left, value)
+                root.pl = self.__Insert(root.pl, value)
         
         return root
 
